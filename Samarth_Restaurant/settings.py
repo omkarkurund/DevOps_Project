89780cdc4dc63ac149d2bc06977839fa308828a3
omkarkurund <omkarkurund98@gmail.com>
2023-10-21 07:35:32 -0700

    yaml file

--- Samarth_Restaurant/settings.py
+++ Samarth_Restaurant/settings.py
@@ -125,17 +125,21 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # managing media
+
+MEDIA_URL="/media/"
+
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 
 # STATIC_URL = '/static/'
 
-MEDIA_URL="/media/"
+
 
 
 
 #Added code
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
+   os.path.join(BASE_DIR, 'static'),  # Include your project-specific static directory
+    '/path/to/third-party/static/',
 ]
 
 
